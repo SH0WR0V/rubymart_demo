c23d5d09f822df5e6aaa2ed998aac0ca5616ee41
SH0WR0V <sm.shahriar1230@gmail.com>
2024-10-10 16:55:05 +0600

    commited

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cardzglobal</title>
+    <link
+    href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
+    rel="stylesheet"
+    />
+    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+</head>
+<body>
+    <header>
+        <nav class="container">
+            <div>Logo</div>
+            <div>
+                <ul class="nav-list">
+                    <li><a href="#" class="nav-link">Home</a></li>
+                    <li><a href="#about-us" class="nav-link">About</a></li>
+                    <li><a href="#" class="nav-link">Culture</a></li>
+                    <li><a href="#" class="nav-link">Join</a></li>
+                    <li><a href="#" class="nav-link">Business Structure</a></li>
+                    <li><a href="#" class="nav-link">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <section class="feature-list">
+        <div class="container grid grid--4--cols">
+            <div class="feature">
+            <ion-icon name="home-outline" class="feature-icon"></ion-icon>
+              <p class="feature-title">High Quality Clothing</p>
+              <p class="feature-text">
+                All clothes are cut stitched and finished by hand and undergo a five stage quality control procedure to ensure satisfaction. Throughout manufacture all stages are quality checked to ensure the highest standards are met.
+              </p>
+            </div>
+            <div class="feature">
+              <ion-icon name="bag-outline" class="feature-icon"></ion-icon>
+              <p class="feature-title">Fabric Sourcing</p>
+              <p class="feature-text">
+                Fabrics are sourced to match your requirements for composition and feel great texture. We can also dye fabrics to meet your exact colour specifications and give materials additional treatments for softness.
+              </p>
+            </div>
+            <div class="feature">
+              <ion-icon name="clipboard-outline" class="feature-icon"></ion-icon>
+              <p class="feature-title">Prototypes & Sampling</p>
+              <p class="feature-text">
+                For each order we create a prototype for your approval, allowing you to test and refine the functionality of your design. Once your samples have been approved, we will commence bulk manufacture.
+              </p>
+            </div>
+            <div class="feature">
+              <ion-icon name="checkmark-circle-outline" class="feature-icon"></ion-icon>
+              <p class="feature-title">Bulk Manufacture</p>
+              <p class="feature-text">
+                Bulk manufacture takes place on our production line, where all items are made for order and quality checked throughout. Quality checks also take place in the finishing stages, before your items are packaged for delivery.
+              </p>
+            </div>
+          </div>
+    </section>
+
+
+    <section class="gallery-list">
+        <div class="container">
+            <h2 class="heading-secondary">
+                Mash Fabric Lace
+            </h2>
+          </div>
+        <div class="container grid grid--4--cols">
+            <div class="feature">
+            <img src="lace-1.jpg" alt="" class="gallery-image">
+              <p class="feature-title">Advantage One</p>
+              <p class="feature-text">
+                Plentiful of new designs, each season we make up over 100 styles of new lace to meet customer’s needs for fashion.
+              </p>
+            </div>
+            <div class="feature">
+              <img src="lace-2.jpg" alt="" class="gallery-image">
+              <p class="feature-title">Advantage Two</p>
+              <p class="feature-text">
+                High quality to ensure your goods quality so as to get more orders from satisfied buyers.
+              </p>
+            </div>
+            <div class="feature">
+              <img src="lace-3.jpg" alt="" class="gallery-image">
+              <p class="feature-title">Advantage Three</p>
+              <p class="feature-text">
+                In time and sincere response to any of your queries
+              </p>
+            </div>
+            <div class="feature">
+              <img src="lace-4.jpg" alt="" class="gallery-image">
+              <p class="feature-title">Advantage Four</p>
+              <p class="feature-text">
+                High quality to ensure your goods quality so as to get more orders from satisfied buyers.
+              </p>
+            </div>
+          </div>
+    </section>
+
+
+    <section class="lace-style-list">
+        <div class="container">
+            <h2 class="heading-secondary">
+                Lace Styles
+            </h2>
+          </div>
+        <div class="container grid grid--4--cols">
+            <div class="feature">
+            <img src="lace-style-1.jpg" alt="" class="lace-style-image">
+              <p class="feature-title">Advantage One</p>
+              <p class="feature-text">
+                Plentiful of new designs, each season we make up over 100 styles of new lace to meet customer’s needs for fashion.
+              </p>
+            </div>
+            <div class="feature">
+              <img src="lace-style-2.jpg" alt="" class="lace-style-image">
+              <p class="feature-title">Advantage Two</p>
+              <p class="feature-text">
+                High quality to ensure your goods quality so as to get more orders from satisfied buyers.
+              </p>
+            </div>
+            <div class="feature">
+              <img src="lace-style-3.jpg" alt="" class="lace-style-image">
+              <p class="feature-title">Advantage Three</p>
+              <p class="feature-text">
+                In time and sincere response to any of your queries
+              </p>
+            </div>
+            <div class="feature">
+              <img src="lace-style-4.jpg" alt="" class="lace-style-image">
+              <p class="feature-title">Advantage Four</p>
+              <p class="feature-text">
+                In time and sincere response to any of your queries
+              </p>
+            </div>
+          </div>
+    </section>
+
+    <section class="about-us" id="about-us">
+        <div class="container grid grid--2--cols">
+            <!-- Step - 01 -->
+  
+            <div class="step-text-box">
+              <h2 class="about-heading-secondary">
+                About Us
+              </h2>
+              <p class="about-text">
+                Aimed at assisting the fashion makers to delight their designs and customers, Cardzglobal lace provides a full solution to the fashion decorations.
+              </p>
+              <p class="about-text">
+                We’ve been working in lace industry for 15 years, where we developed our design creativity, our strong source for the fine raw materials, our control to the dyeing section and a wide net of customer support. Intime and responsible feedback is our mission. Reasonable price and non-complaint quality are our essence for long-term standing. We welcome friends around the world who need the fashions solution and worthwhile quality and service. We shall be devoted to being your best choice and partner.
+              </p>
+            </div>
+            <div class="step-img-box">
+              <img
+                src="lace-5.jpg"
+                class="about-us-image"
+                alt=""
+              />
+            </div>
+    </section>
+
+    <section class="section-featured">
+        <div class="container-featured">
+          <h2 class="heading-featured-in">OUR CLIENTS</h2>
+          <div class="logos">
+            <img src="techcrunch.png" alt="techcrunch logo" />
+            <img
+              src="business-insider.png"
+              alt="business-insider logo"
+            />
+            <img
+              src="the-new-york-times.png"
+              alt="the-new-york-times logo"
+            />
+            <img src="forbes.png" alt="forbes logo" />
+            <img src="usa-today.png" alt="usa-today logo" />
+          </div>
+        </div>
+      </section>
+
+      <footer class="footer">
+        <div class="container">
+            <h2 class="heading-secondary">Contact Us</h2>
+            <div class="contact-info">
+                <div>
+                    <h3>China Factory</h3>
+                    <a href="mailto:sam@cardzglobal.com.cn">sam@cardzglobal.com.cn</a>
+                </div>
+                <div>
+                    <h3>Hong Kong Factory</h3>
+                    <a href="mailto:hk@cardzglobal.com">hk@cardzglobal.com</a>
+                </div>
+                <div>
+                    <h3>France Office</h3>
+                    <a href="mailto:fr@cardzglobal.com">fr@cardzglobal.com</a>
+                </div>
+                <div>
+                    <h3>Bangladesh Factory</h3>
+                    <a href="mailto:bd@cardzglobal.com">bd@cardzglobal.com</a>
+                </div>
+            </div>
+            <div class="copyright">
+                Copyright © 2024 Cardzglobal Limited. All Rights Reserved.
+            </div>
+        </div>
+    </footer>
+
+    <script
+      type="module"
+      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+    ></script>
+    <script
+      nomodule
+      src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+    ></script>
+</body>
+</html>
